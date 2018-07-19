80ff65692f8710a1f13f298e06cdd4b7be7b2757
an0e <an0e@WL10PLD12>
2018-07-19 12:03:03 +1000

    Adds Scss + fonts

--- projects/component-library/src/lib/components/item/item.component.ts
+++ projects/component-library/src/lib/components/item/item.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
 
